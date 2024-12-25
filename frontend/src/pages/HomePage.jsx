e1815af4467983baf86f77344a2710d0f13d269a
Abdillahi Haji <hajiabdi213@gmail.com>
2024-12-25 17:39:14 +0300

    Enhance HomePage with introductory content and navigation link to Search

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 py-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span> <br />{" "}
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Iftiinshe Estate is the best place to find your next perfect place to
+          live <br /> we have a wide reange of properties for you to choose from
+        </div>
+        <Link
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+          to={"/search"}
+        >
+          Let's get started ....
+        </Link>
+      </div>
+
+      {/* swiper */}
+
+      {/* listing results for offer, sale and rent */}
+    </div>
+  );
 };
 
 export default HomePage;
