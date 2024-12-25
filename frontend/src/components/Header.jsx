640549abbb7700dfe195bb66395f17552d766e07
Abdillahi Haji <hajiabdi213@gmail.com>
2024-12-25 09:06:01 +0300

    Implement search functionality in Header component with URL parameter handling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,28 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useUserContext } from "../context/AppContext";
-
-// import useUserStore from "../store/user.store";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  // const { currentUser } = useUserStore();
-  // console.log(currentUser);
+  const location = useLocation();
+  const navigate = useNavigate();
   const { currentUser } = useUserContext();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200  shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -21,13 +36,20 @@ const Header = () => {
         </Link>
 
         {/* search */}
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
           />
-          <FaSearch className="text-slate-500" />
+          <button>
+            <FaSearch className="text-slate-500" />
+          </button>
         </form>
 
         {/* menu */}
