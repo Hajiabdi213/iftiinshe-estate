42bdf2ba17559bce93a8ba9f83f20ea621fdd896
Abdillahi Haji <hajiabdi213@gmail.com>
2024-12-25 17:59:17 +0300

    Refactor database connection to use environment variable and update package dependencies

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const connection = await mongoose
-      .connect(
-        "mongodb+srv://abdillahi:tijaabo123@mern-real-estate-projec.nmrdn.mongodb.net/?retryWrites=true&w=majority&appName=mern-real-estate-project"
-      )
+      .connect(process.env.MONGO_URI)
       .then(() => console.log("Connected to the MongoDB"));
   } catch (error) {
     console.log(error.message);
